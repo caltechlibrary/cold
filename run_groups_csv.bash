7847b79a2d9990cc053a430a4b890a344b5602e8
R. S. Doiel <rsdoiel@gmail>
2024-10-24 17:22:55 -0700

    Quick Save

---
+++ run_groups_csv.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+# FIXME: this report needs to SSH to the data processing system and then run the rpt_groups_csv.bash script.
+
+# Mockup report being run
+sleep 5;
+echo "files://dataset.library.caltech.edu/Sites/feeds_v1.6/groups.csv"
---
+++ run_people_csv.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+# FIXME: this report needs to SSH to the data processing system and then run the rpt_people_csv.bash script.
+
+# Mockup report being run
+sleep 15;
+echo "files://dataset.library.caltech.edu/Sites/feeds_v1.6/people.csv"
