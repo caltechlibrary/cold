a80a3083509e677299a1d8e72427db42bad8d1bb
R. S. Doiel <rsdoiel@gmail>
2024-11-04 13:00:58 -0800

    added script to push in automatic reports

---
+++ push_completed_report.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#
+# Push compelted report takes a report name and link path and creates/replaces the
+# record in the reports.ds collection.
+#
+
+
+
+#{
+#    "content_type": null,
+#    "emails": "rsdoiel@localhost",
+#    "id": "842482ce-6985-5440-8820-871ebf07928b",
+#    "link": "rpt/people.csv",
+#    "report_name": "run_people_csv",
+#    "requested": "2024-11-04T18:45:11.197Z",
+#    "status": "completed",
+#    "updated": "2024-11-04T18:45:17.122Z"
+#}
+function push_report() {
+	report_name="$1"
+	report_link="$2"
+	timestamp="$(date +"%Y-%m-%d %H:%M:%S%z")"
+	dataset delete reports.ds "$report_name"
+	cat <<JSON | dataset create -i - reports.ds "${report_name}"
+{
+    "content_type": "text/csv",
+    "emails": "",
+    "id": "${report_name}",
+    "link": "${report_link}",
+    "report_name": "${report_name}",
+    "requested": "$timestamp",
+    "status": "completed",
+    "updated": "$timestamp"
+}
+JSON
+}
+
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+	echo "USAGE: $0 REPORT_NAME REPORT_LINK"
+	exit 1
+fi
+push_report "$1" "$2"
