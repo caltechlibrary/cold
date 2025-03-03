51cc0f95bc235468ecf579b3f60317db35114789
R. S. Doiel <rsdoiel@gmail>
2025-03-03 15:44:54 -0800

    added division_people load/dump cli

---
+++ division_people.ts
@@ -0,0 +1,83 @@
+/**
+ * division_people.ts is a program designed to take a CSV file with columns organized
+ * as
+ * `"division","clpid","orcid","family_name","given_name","other group","other group", ...
+ * and import the list of people updating their groups memberships based on the "division" column and
+ * any column labeld "other group".
+ */
+import { licenseText, releaseDate, releaseHash, version } from "./deps.ts";
+import * as cli from "@std/cli";
+import { dumpDivisionPeopleCSV, loadDivisionPeopleCSV } from "./utils.ts";
+
+import { divisionPeopleHelpText, fmtHelp } from "./helptext.ts";
+
+const appName = "division_people";
+
+async function main() {
+  const app = cli.parseArgs(Deno.args, {
+    alias: {
+      help: "h",
+      license: "l",
+      version: "v",
+    },
+    default: {
+      help: false,
+      version: false,
+      license: false,
+    },
+  });
+  if (app.help) {
+    console.log(
+      fmtHelp(
+        divisionPeopleHelpText,
+        appName,
+        version,
+        releaseDate,
+        releaseHash,
+      ),
+    );
+    Deno.exit(0);
+  }
+  if (app.version) {
+    console.log(`${appName} ${version} ${releaseHash}`);
+    Deno.exit(0);
+  }
+  if (app.license) {
+    console.log(`${licenseText}`);
+    Deno.exit(0);
+  }
+  const args = app._;
+  if (args.length === 0) {
+    console.log(
+      fmtHelp(
+        divisionPeopleHelpText,
+        appName,
+        version,
+        releaseDate,
+        releaseHash,
+      ),
+    );
+    Deno.exit(1);
+  }
+  if (args.length !== 2) {
+    console.log(`USAGE: ${appName} load|dump CSV_FILENAME`);
+    Deno.exit(1);
+  }
+  const cmd = args[0];
+  const csvFilename: string = args[1] as unknown as string;
+  let err: string = "";
+  switch (cmd) {
+    case "load":
+      err = await loadDivisionPeopleCSV(csvFilename);
+      break;
+    case "dump":
+      err = await dumpDivisionPeopleCSV(csvFilename);
+      break;
+  }
+  if (err !== "") {
+    console.error(err);
+    Deno.exit(1);
+  }
+}
+
+if (import.meta.main) main();
